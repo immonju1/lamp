dc022ada64ccd4194ffc4270f24b70f4bcb4608c
Juha Immonen <jmi@kolumbus.fi>
2018-11-27 14:21:00 +0000

    bug fixed

--- salt/apache/connection.php
+++ salt/apache/connection.php
@@ -4,7 +4,6 @@ $database = "animals";
 $user = "animals";
 $password = "salainen";
 $dns = "mysql:host=localhost;charset=UTF8;dbname=$database";
-echo "$dns, $user";
 $con = new PDO($dns,$user,$password);
 
 ?>
